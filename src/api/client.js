7b1757d1cc7c97baef0bcb5b720242400774b272
patrick simon <patricksimonogar@gmail.com>
2026-08-27 04:39:59 +0100

    fix-leadership-image-asset-resolution

--- src/api/client.js
+++ src/api/client.js
@@ -23,6 +23,10 @@ export function assetUrl(value) {
     cleanPath = '/' + cleanPath
   }
 
+  if (typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')) {
+    return cleanPath
+  }
+
   const apiBase = api.defaults.baseURL || '/api'
   if (/^https?:\/\//i.test(apiBase)) {
     const origin = new URL(apiBase).origin
