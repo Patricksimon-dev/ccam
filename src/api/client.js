ffa916becc617a1b003985684da2e1f658ce0994
patrick simon <patricksimonogar@gmail.com>
2026-08-27 02:39:26 +0100

    just little

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const TOKEN_KEY = 'church_token'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? '/api' : 'https://christchosen.onrender.com/api'),
+  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.DEV ? '/api' : 'https://cc-cosp.onrender.com/api'),
   headers: { 'Content-Type': 'application/json' },
 })
 
